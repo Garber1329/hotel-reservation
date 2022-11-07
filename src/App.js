22cdd2a655c0fd12bba2a68a1ba8b4d401332055
Vitalii Chaikovskyi <garber1329@gmail.com>
2022-11-07 21:52:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.getItem('auth')) {
+      setIsAuth(true)
+    }
+    setLoading(false);
+  }, [])
+
+  return (
+      <AuthContext.Provider value={{
+        isAuth,
+        setIsAuth,
+        isLoading
+      }}>
+        <BrowserRouter>
+          <Navbar/>
+          <AppRouter/>
+          <Footer/>
+        </BrowserRouter>
+      </AuthContext.Provider>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
